da359ae72973feccf3ae830d1290628c3ed6d567
nonoumasy <nonoumasy@gmail.com>
2021-04-01 15:51:20 -0700

    init commit

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+const About = () => {
+    return (
+        <div>
+            <h1>
+                About
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio autem repellendus, architecto quaerat facere minima voluptates eligendi asperiores delectus laudantium eaque accusantium earum aperiam doloribus! Asperiores, autem tempore? Harum, consequatur?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio autem repellendus, architecto quaerat facere minima voluptates eligendi asperiores delectus laudantium eaque accusantium earum aperiam doloribus! Asperiores, autem tempore? Harum, consequatur?
+            </p>
+        </div>
+      );
+}
+ 
+export default About;
---
+++ pages/tips/index.js
@@ -0,0 +1,11 @@
+const Tips = () => {
+    return ( 
+        <div>
+            <h2>
+                Travel Tips
+            </h2>
+        </div>
+     );
+}
+ 
+export default Tips;
