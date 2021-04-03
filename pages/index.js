971d82ed3e455380bf1c3effba946f06b5f49ab4
nonoumasy <nonoumasy@gmail.com>
2021-04-02 18:49:31 -0700

    asdf

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import Tips from '../components/Tips'
 
-import firebase from '../pages/firebase'
+import firebase from '../components/firebase'
 
 export const getStaticProps = async () => {
 
@@ -19,15 +20,13 @@ export const getStaticProps = async () => {
     console.log('lisa', result)
     // const data = result.json()
     return {
-      props: { tips: result}
-      }
+      props: { tips: result }
+    }
   } catch (error) {
     console.error(error);
     throw error;
   }
 }
-
-
 
 export default function Home({tips}) {
   return (
@@ -37,17 +36,7 @@ export default function Home({tips}) {
         <meta name='keywords' content='travel tips'/>
       </Head>
 
-      {tips?.map(item => (
-        <div key={item.id} style={{width: 600, marginTop: 40,}}>
-          <div>
-            <img src={item?.userImage} alt="" style={{ width: 30, height: 30, borderRadius: 50, objectFit: 'cover'}} /> {item.user}
-          </div>
-          <h2>{item?.title}</h2>
-          <img src={item?.image} alt="" style={{ width: 600, height: 400, objectFit: 'cover'}}/>
-          <div style={{marginTop: 20,}}>{item.tip}</div>
-        </div>
-        
-      ))}
+      <Tips tips={tips}/>
     </>
   )
 }
