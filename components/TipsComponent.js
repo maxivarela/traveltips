503b657048cb1045c8409dcc1551707018051815
nonoumasy <nonoumasy@gmail.com>
2021-04-03 22:47:53 -0700

    styling

--- components/TipsComponent.js
+++ components/TipsComponent.js
@@ -6,7 +6,12 @@ import Image from 'next/image';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     AppBar,
+    Avatar,
     Button,
+    Card, 
+    CardActions,
+    CardContent,
+    CardHeader,
     IconButton,
     Toolbar,
     Typography,
@@ -19,104 +24,106 @@ const TipsComponent = ({tips}) => {
     return ( 
         <>
             {tips?.map(item => (
-                <div
+                <Card
                     key={item.id}
                     className={"Card"}
+                    style={{marginBottom: 40,}}
                     >  
+                    <CardHeader 
+                        avatar={
+                            <Avatar aria-label="recipe" className={classes.avatar}>
+                                <img src={item?.userImage} alt="" style={{ width: '100%', height: 40, borderRadius: 50, objectFit: 'cover' }} />
+                            </Avatar>} 
+                        title={item.user}
+                        subheader="September 14, 2016"
+                            >
+                    </CardHeader>
 
-                    <div style={{display: 'flex', alignItems: 'center', }}>
-                        <img src={item?.userImage} alt="" style={{ width: 30, height: 30, borderRadius: 50, objectFit: 'cover' }} /> 
-                        <div style={{marginLeft: 10,}}>
-                            {item.user}
-                        </div> 
-                    </div>
-                        
-                    <div style={{marginTop: 20,}}>
-                        {
-                        item?.image 
-                        ?
-                            item?.image?.includes('youtube.com') 
-                            ?
-                            <iframe
-                                style={{
-                                        width: '100%', 
-                                        height: '30vh', 
-                                        borderRadius: 5,
-                                        borderWidth: '0 !important',
-                                        margin: '0 !important',}}
-                                src={item?.image}
-                                allowFullScreen
-                                mozallowfullscreen="mozallowfullscreen"
-                                msallowfullscreen="msallowfullscreen"
-                                oallowfullscreen="oallowfullscreen"
-                                webkitallowfullscreen="webkitallowfullscreen"
-                                allow="accelerometer"
-                                title={item?.image}
-                                type="*"
-                            />
-                            :
-                                    <img 
-                                        src={item?.image} 
-                                        alt='' 
-                                        style={{ 
-                                            width: '100%', 
-                                            height: '30vh', 
-                                            objectFit: 'cover',
-                                            borderRadius: 5, }}
-                                        />
-                        : 
-                        []
-                        }   
-                    </div>
                     <div>
-                        {item?.audio 
+                        {
+                            item?.image
+                                ?
+                                item?.image?.includes('youtube.com')
+                                    ?
+                                    <iframe
+                                        style={{
+                                            width: '100%',
+                                            height: '30vh',
+                                            borderWidth: '0 !important',
+                                            margin: '0 !important',
+                                        }}
+                                        src={item?.image}
+                                        allowFullScreen
+                                        mozallowfullscreen="mozallowfullscreen"
+                                        msallowfullscreen="msallowfullscreen"
+                                        oallowfullscreen="oallowfullscreen"
+                                        webkitallowfullscreen="webkitallowfullscreen"
+                                        allow="accelerometer"
+                                        title={item?.image}
+                                        type="*"
+                                    />
+                                    :
+                                    <img
+                                        src={item?.image}
+                                        alt=''
+                                        style={{
+                                            width: '100%',
+                                            height: '30vh',
+                                            objectFit: 'cover',
+                                        }}
+                                    />
+                                :
+                                []
+                        }
+                    </div>
+                    
+                    {item?.audio
                         ?
+                        <CardContent>
                             <audio controls style={{ width: '100%' }}>
                                 <source src={item?.audio} type="audio/ogg" />
                                 <source src={item?.audio} type="audio/mpeg" />
                             </audio>
+                        </CardContent>
                         :
                         null
-                        }
-                        
-                    </div>
+                    }
 
-                    <div>
-                        <div style={{ margin: '20px auto', fontSize: 20}}>
+                    <CardContent>
+                        <Typography variant='h6' component='h2' style={{marginBottom: 10}}>
                             {item?.title}
-                        </div>
-                    </div>
-                    
-                    <div >
-                        <div style={{ margin: '20px auto', }}>
-                            {item?.description.substring(0,300)}
-                        </div>
-                    </div>
-
-                    <div className={classes.tagsContainer}>
-                        {item?.tags?.map(tag=> {
+                        </Typography>
+                        <Typography >
+                            {item?.description.substring(0, 300)}
+                        </Typography>
+                    </CardContent>
+                    <CardContent>
+                        {item?.tags?.map(tag => {
                             return (
                                 <div style={{
-                                    marginBottom: 10,
                                     display: 'inline-block',
                                     textAlign: 'left',
                                     fontSize: '14px',
                                     fontWeight: 400,
                                     marginRight: 5,
                                     color: '#26978A',
-                                    textTransform: 'capitalize',}} key={tag}>
+                                    textTransform: 'capitalize',
+                                }} key={tag}>
                                     {(tag != "") && tag + ','}
                                 </div>
                             )
                         })}
-                    </div>
-                    <Link 
-                        href={`./tips/${item.id}`}
-                        style={{ pointer: 'cursor', marginTop: 20,}}
-                        >
-                        MORE DETAILS
-                    </Link>
-                </div>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <Link
+                            href={`./tips/${item.id}`}
+                            >
+                            <Button>
+                                MORE DETAILS
+                            </Button>
+                        </Link>
+                    </CardActions>
+                </Card>
             ))}
         </>
     );
