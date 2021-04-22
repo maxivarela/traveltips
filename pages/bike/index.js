759053342ba2c73bff31f4e17468b681b58e590b
nonoumasy <nonoumasy@gmail.com>
2021-04-21 22:35:59 -0700

    sections now work

--- pages/bike/index.js
+++ pages/bike/index.js
@@ -1,7 +1,32 @@
-export default function BikeTravel() {
+import Head from 'next/head'
+import TipsComponent from '../../components/TipsComponent'
+import firebase from '../../components/firebase'
+
+//this runs in build time. don't put code here that you expect to run in browser
+export const getStaticProps = async () => {
+    try {
+        const res = await firebase.firestore().collection('tips').where('tags', 'array-contains', "bike").orderBy('createdAt', 'desc').limit(10).get()
+        const data = await res.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        return {
+            props: { data },
+            revalidate: 10,
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export default function BikeTravel({ data }) {
     return (
-        <div>
-            Bike Travel sections
+        <div style={{ padding: '1rem', }}>
+            <Head>
+                <title>Bike Travel Tips</title>
+                <meta name='keywords' content='travel tips' />
+            </Head>
+            Bike Travel Section
+
+            <TipsComponent tips={data} />
         </div>
     )
 }
--- pages/women/index.js
+++ pages/women/index.js
@@ -1,7 +1,32 @@
-export default function WomenTravel() {
+import Head from 'next/head'
+import TipsComponent from '../../components/TipsComponent'
+import firebase from '../../components/firebase'
+
+//this runs in build time. don't put code here that you expect to run in browser
+export const getStaticProps = async () => {
+    try {
+        const res = await firebase.firestore().collection('tips').where('tags', 'array-contains', "women").orderBy('createdAt', 'desc').limit(10).get()
+        const data = await res.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        return {
+            props: { data },
+            revalidate: 10,
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export default function WomenTravel({ data }) {
     return (
-        <>
-            Women Travel section
-        </>
+        <div style={{ padding: '1rem', }}>
+            <Head>
+                <title>Women Travel Tips</title>
+                <meta name='keywords' content='travel tips' />
+            </Head>
+            Women Travel Section
+
+            <TipsComponent tips={data} />
+        </div>
     )
 }
