6ca54aa55955558057e487e26f9017b5d7568e10
nonoumasy <nonoumasy@gmail.com>
2021-04-21 20:31:23 -0700

    added drawer

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,44 +1,199 @@
+import {useState} from "react";
 import Link from 'next/link';
+import clsx from "clsx";
 
-import { makeStyles } from '@material-ui/core/styles';
+const drawerWidth = 300;
+
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import {
     AppBar,
     Button,
+    CssBaseline,
+    Divider,
+    Drawer,
     IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
     TextField,
     Toolbar,
     Typography,
 } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import InfoIcon from '@material-ui/icons/Info';
+import GroupIcon from '@material-ui/icons/Group';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 
 export default function Navbar() {
     const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
     return (
-        <>
-            <AppBar position="static" elevation={0}>
-                <Toolbar style={{ backgroundColor: '#111', }}>
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="static"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open
+                })}
+            >
 
-                    <div style={{ flexGrow: 1,}}>
+                <Toolbar style={{ backgroundColor: '#111', }}>
+                    <div style={{ flexGrow: 1, }}>
                         <Link href='/'>
                             <Button color="inherit" >
-                                <Typography variant="h5" component="h1" >
+                                <Typography variant="h5" component="h1">
                                     <span style={{ color: '#009688' }}>Trip</span>Tips
-                                </Typography>
+                            </Typography>
                             </Button>
                         </Link>
                     </div>
                     
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        onClick={handleDrawerOpen}
+                        className={clsx(open && classes.hide)}
+                        >
                         <MenuIcon />
                     </IconButton>
+
                 </Toolbar>
             </AppBar>
-        </>
+
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="right"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper
+                }}
+                >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === "rtl" ? (
+                            <ChevronLeftIcon />
+                        ) : (
+                            <ChevronRightIcon />
+                        )}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"Inbox"} />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <ChatBubbleOutlineIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"Live Chat"} />
+                    </ListItem>
+                </List>
+                <Divider />
+                <List>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <InfoIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"About"} />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <GroupIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"Team"} />
+                    </ListItem>
+                </List>
+                <Divider />
+
+                <List>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <ExitToAppIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"Login"} />
+                    </ListItem>
+                </List>
+            </Drawer>
+
+        </div>
+            
     );
 }
 
 const useStyles = makeStyles((theme) => ({
-
+    root: {
+        display: "flex"
+    },
+    appBar: {
+        transition: theme.transitions.create(["margin", "width"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        })
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["margin", "width"], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen
+        }),
+        marginRight: drawerWidth
+    },
+    title: {
+        flexGrow: 1
+    },
+    hide: {
+        display: "none"
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    drawerHeader: {
+        display: "flex",
+        alignItems: "center",
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: "flex-start"
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        marginRight: -drawerWidth
+    },
+    contentShift: {
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen
+        }),
+        marginRight: 0
+    }
 }));
