6de021af4649584b4fcafcf402b5be862dc29879
nonoumasy <nonoumasy@gmail.com>
2021-04-26 07:26:51 -0700

    profile page

---
+++ pages/profile/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Profile() {
+    return (
+        <div style={{ padding: '1rem', }}>
+            <Head>
+                <title>Profile Page</title>
+                <meta name='keywords' content='travel tips' />
+            </Head>
+            <h2>
+                Profile Page
+            </h2>
+            
+        </div>
+    )
+}
