e2c3bec940a98f61781c49a94c7d47cfad9ceb1f
nonoumasy <nonoumasy@gmail.com>
2021-04-26 07:38:06 -0700

    added back button

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,28 +1,41 @@
 import Head from 'next/head'
 import TipsComponent from '../../components/TipsComponent'
 import { getTipsBySearch } from '../api'
+import { useRouter } from 'next/router'
 
-const searchTerm='lisbon'
+import {
+    Button,
+    Typography,
+} from '@material-ui/core';
+
 //this runs in build time. don't put code here that you expect to run in browser
 export const getServerSideProps = async (context) => {
     const {search} = await context.query
     try {
         const data = await getTipsBySearch(search)
-        return data
+        return search,data
     } catch (error) {
         throw error;
     }
 }
 
-export default function Search({ data }) {
+export default function Search({ search, data }) {
+
+    const router = useRouter()
+    console.log('joi', data, search)
     return (
         <div style={{ padding: '1rem', }}>
             <Head>
                 <title> Travel Tips</title>
                 <meta name='keywords' content='travel tips' />
             </Head>
+            <Button onClick={() => router.back()}>
+                <Typography color='primary' style={{ fontSize: 12, }}>
+                    Back
+                </Typography>
+            </Button>
             <h2>
-                Search Results
+                Search Results for "{search}" : {data?.length} items
             </h2>
 
             <TipsComponent tips={data} />
