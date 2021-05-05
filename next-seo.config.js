ac4f8a7c26aa9c775c8fa70393658c18df65a5a0
nonoumasy <nonoumasy@gmail.com>
2021-05-04 18:31:19 -0700

    asdfds

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Travel tips and tricks.',
-    description: 'TripTips is a social platform to create, explore, and share travel tips.Unique travel trips. Travel tips and tricks. Travel tips for beginners. Travel tips during covid. Travel tips 2020. Fun travel tips. Travel tips packing.5 travel tips.',
+    description: 'TripTips is a social platform to create, explore, and share travel tips.Unique travel trips. Travel tips and tricks. Travel tips for beginners. Travel tips during covid. Travel tips 2020. Fun travel tips. Travel tips packing.5 travel tips.',
     openGraph: {
         type: 'website',
         locale: 'en_US',
