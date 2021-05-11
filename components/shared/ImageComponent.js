8929581fc64b9b10d0275e00c01e2740cccf71b3
nonoumasy <nonoumasy@gmail.com>
2021-05-10 18:11:28 -0700

    adsfds

--- components/shared/ImageComponent.js
+++ components/shared/ImageComponent.js
@@ -13,23 +13,23 @@ const ImageComponent = ({ image, title, youtubeId }) => {
             ?
                 image && image.includes('youtube.com')
                     ?
-                    <Image src={`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`} alt={title} width={'400'} height={'300'}/>
-                    // <iframe
-                    //     className={classes.video}
-                    //     // this loads imageThumbnail instead of the Youtube embedded video which isnt optimal.
-                    //     srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;}</style><a href=${image}><img src=${`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`} alt=${title}><span>▶</span></a>`}
-                    //     src={image}
-                    //     frameBorder="0"
-                    //     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    //     allowFullScreen
-                    //     mozallowfullscreen="mozallowfullscreen"
-                    //     msallowfullscreen="msallowfullscreen"
-                    //     oallowfullscreen="oallowfullscreen"
-                    //     webkitallowfullscreen="webkitallowfullscreen"
-                    //     loading="lazy"
-                    //     title={image}
-                    //     type="*"
-                    // ></iframe>
+                    // <Image src={`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`} alt={title} width={'400'} height={'300'}/>
+                    <iframe
+                        className={classes.video}
+                        // this loads imageThumbnail instead of the Youtube embedded video which isnt optimal.
+                        srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;}</style><a href=${image}><img src=${`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`} alt=${title}><span>▶</span></a>`}
+                        src={image}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        mozallowfullscreen="mozallowfullscreen"
+                        msallowfullscreen="msallowfullscreen"
+                        oallowfullscreen="oallowfullscreen"
+                        webkitallowfullscreen="webkitallowfullscreen"
+                        loading="lazy"
+                        title={image}
+                        type="*"
+                    ></iframe>
                     :
                     image && 
                     <Image src={image} width={'400'} height={'300'} alt={title?.substring(0,50)} className={classes.media}/>
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
-import theme from '../src/theme';
 import { GA_TRACKING_ID } from '../lib/gtag'
-
 export default class MyDocument extends Document {
     render() {
         return (
