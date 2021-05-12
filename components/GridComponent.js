a14d8b378a333c486ae6df028dbcbd6579c8b5dd
nonoumasy <nonoumasy@gmail.com>
2021-05-11 21:15:29 -0700

    dfasf

--- components/GridComponent.js
+++ components/GridComponent.js
@@ -1,31 +1,20 @@
 import {CardComponent} from './CardComponent';
-import Skeleton from '@material-ui/lab/Skeleton'
 
 import {
     Grid,
 } from '@material-ui/core';
 
-export default function GridComponent ({ tips, locale, locales}) {
+export default function GridComponent ({ tips, locale,}) {
 
     return (
         <Grid container spacing={3}>
                 {tips?.map(item => {
                     return (
                         <Grid item key={item.id} xs={12} sm={6} md={4} lg={3} xl={3}>
-                            {item 
-                            ? 
                             <CardComponent item={item} locale={locale} /> 
-                            : 
-                            <>
-                                <Skeleton height='220px'/>
-                                <Skeleton/>
-                                <Skeleton/>
-                            </>
-                            }
                         </Grid>
                     )
                 })}
-
         </Grid>
     );
 }
