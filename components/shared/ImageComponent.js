1cd4f01c41785100ae0706fb7b3013c59bde0874
nonoumasy <nonoumasy@gmail.com>
2021-05-13 03:52:13 -0700

    adsfas

--- components/shared/ImageComponent.js
+++ components/shared/ImageComponent.js
@@ -10,9 +10,9 @@ const ImageComponent = ({ image, title, youtubeId }) => {
             ?
                 image && image.includes('youtube.com')
                     ?
+                    // this loads imageThumbnail instead of the Youtube embedded video.
                     <iframe
-                        style={{ width: "100%", height: "100%", objectFit: "cover", }}
-                        // this loads imageThumbnail instead of the Youtube embedded video.
+                        style={{ width: "100%", height: "100%" }}
                         srcDoc={`
                         <style>
                             *{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;height:100%;top:0;bottom:0;margin:auto;object-fit:cover;}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;}
@@ -34,14 +34,14 @@ const ImageComponent = ({ image, title, youtubeId }) => {
                     />
                     :
                     image && 
-                    // <Image
-                    //     layout='fill'
-                    //     objectFit='cover'
-                    //     src={image}
-                    //     alt={title?.substring(0, 50)} 
-                    //     style={{ width: "100%", height: "100%" , objectFit: "cover", }}
-                    // />
-                    <img src={image} alt={title?.substring(0, 50)} style={{ width: "100%", height: "100%", objectFit: "cover", }}/>
+                    <Image
+                        layout='fill'
+                        objectFit='cover'
+                        src={image}
+                        alt={title?.substring(0, 50)} 
+                        style={{ width: "100%", height: "100%" , objectFit: "cover", }}
+                    />
+                    // <img src={image} alt={title?.substring(0, 50)} style={{ width: "100%", height: "100%", objectFit: "cover", }}/>
             :
             null
             }   
