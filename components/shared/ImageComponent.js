67bd72d0e5a2b6bb0e207c32d8ef327232f130c4
nonoumasy <nonoumasy@gmail.com>
2021-05-13 03:22:43 -0700

    asfads

--- components/shared/ImageComponent.js
+++ components/shared/ImageComponent.js
@@ -10,42 +10,38 @@ const ImageComponent = ({ image, title, youtubeId }) => {
             ?
                 image && image.includes('youtube.com')
                     ?
-                    <div style={{width: '100%', height: '100%'}}>
-                        <iframe
-                            style={{ width: "100%", height: "100%", objectFit: "cover", }}
-                            // this loads imageThumbnail instead of the Youtube embedded video.
-                            srcDoc={`
-                            <style>
-                                *{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;height:100%;top:0;bottom:0;margin:auto;object-fit:cover;}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;}
-                            </style>
-                                <a href=${image}>
-                                    <img src=${`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`} alt=${title}>
-                                    <span>
-                                        ▶
-                                    </span>
-                                </a>
-                                `}
-                            src={image}
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                            loading="lazy"
-                            title={image}
-                            type="*"
-                        ></iframe>
-
-                    </div>
-                    
+                    <iframe
+                        style={{ position:'relative', width: "100%", height: "100%", objectFit: "cover", }}
+                        // this loads imageThumbnail instead of the Youtube embedded video.
+                        srcDoc={`
+                        <style>
+                            *{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;height:100%;top:0;bottom:0;margin:auto;object-fit:cover;}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;}
+                        </style>
+                            <a href=${image}>
+                                <img src=${`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`} alt=${title}>
+                                <span>
+                                    ▶
+                                </span>
+                            </a>
+                            `}
+                        src={image}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        loading="lazy"
+                        title={image}
+                        type="*"
+                    />
                     :
                     image && 
-                    // <Image
-                    //     layout='fill'
-                    //     objectFit='cover'
-                    //     src={image}
-                    //     alt={title?.substring(0, 50)} 
-                    //     style={{ width: "100%", height: "100%" , objectFit: "cover", }}
-                    // />
-                    <img src={image} alt={title?.substring(0, 50)} style={{ width: "100%", height: "100%", objectFit: "cover", }}/>
+                    <Image
+                        layout='fill'
+                        objectFit='cover'
+                        src={image}
+                        alt={title?.substring(0, 50)} 
+                        style={{ width: "100%", height: "100%" , objectFit: "cover", }}
+                    />
+                    // <img src={image} alt={title?.substring(0, 50)} style={{ width: "100%", height: "100%", objectFit: "cover", }}/>
             :
             null
             }   
