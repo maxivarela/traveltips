ad35ef44c608b7666f5ff5dcfd6ba9d25b27820a
nonoumasy <nonoumasy@gmail.com>
2021-05-13 02:37:50 -0700

    sadfa

--- components/shared/ImageComponent.js
+++ components/shared/ImageComponent.js
@@ -15,17 +15,17 @@ const ImageComponent = ({ image, title, youtubeId }) => {
                     <iframe
                         style={{ width: "100%", height: "100%", objectFit: "cover", }}
                         // this loads imageThumbnail instead of the Youtube embedded video.
-                        // srcDoc={`
-                        //     <style>
-                        //         *{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;height:100%;top:0;bottom:0;margin:auto;object-fit:cover;}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;}
-                        //     </style>
-                        //         <a href=${image}>
-                        //             <img src=${`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`} alt=${title}>
-                        //             <span>
-                        //                 ▶
-                        //             </span>
-                        //         </a>
-                        //         `}
+                        srcDoc={`
+                            <style>
+                                *{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;height:100%;top:0;bottom:0;margin:auto;object-fit:cover;}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;}
+                            </style>
+                                <a href=${image}>
+                                    <img src=${`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`} alt=${title}>
+                                    <span>
+                                        ▶
+                                    </span>
+                                </a>
+                                `}
                         src={image}
                         frameBorder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
@@ -36,15 +36,15 @@ const ImageComponent = ({ image, title, youtubeId }) => {
                     ></iframe>
                     :
                     image && 
-                    <Image
-                        layout='fill'
-                        objectFit='cover'
-                        src={image}
-                        alt={title?.substring(0, 50)} 
-                        style={{ width: "100%", height: "100%" , objectFit: "cover", }}
-                    />
+                    // <Image
+                    //     layout='fill'
+                    //     objectFit='cover'
+                    //     src={image}
+                    //     alt={title?.substring(0, 50)} 
+                    //     style={{ width: "100%", height: "100%" , objectFit: "cover", }}
+                    // />
                     
-                    // <img src={image} width={'400'} height={'300'} alt={title?.substring(0,50)} className={classes.image}/>
+                    <img src={image} alt={title?.substring(0, 50)} style={{ width: "100%", height: "100%", objectFit: "cover", }}/>
                 
             :
             null
