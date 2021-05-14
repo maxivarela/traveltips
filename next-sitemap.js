dae3d2ef85e2e1909bf33136344277ac8a614737
nonoumasy <nonoumasy@gmail.com>
2021-05-13 21:39:11 -0700

    asdf

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,10 @@
 module.exports = {
     siteUrl: 'https://hacktravels.com/',
     changefreq: 'daily',
+    priority: 0.7,
     generateRobotsTxt: true,
+    sitemapSize: 10000,
+    exclude: ['/404', '/add', '/edit', '/signin'],
     robotsTxtOptions: {
         policies: [
             {
